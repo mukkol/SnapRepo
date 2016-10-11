45ae174716cd04ea26933f74cd3bf03223dbc309
mikkohu <mikko.huilaja@solita.fi>
2016-10-11 16:05:26 +0300

    InitializationModule to get more control of initialization process

---
+++ WebSite/Common/InitializationModule.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using AzureBackupManager.Common.IoC;
+using AzureBackupManager.Scheduling;
+using FluentScheduler;
+using Microsoft.Web.Administration;
+
+namespace AzureBackupManager.Common
+{
+    public class InitializationModule : IHttpModule
+    {
+        private static LogService _logService;
+        public static string MonitorHostUrl;
+
+        public static void ApplicationStart()
+        {
+            ObjectFactory.InitIoC();
+            _logService = ObjectFactory.Container.GetInstance<LogService>();
+            _logService.WriteLog("Application started!", true);
+
+            MonitorHostUrl = GetCurrentSiteHostAddresses().FirstOrDefault();
+            _logService.WriteLog("MonitorHostUrl: " + MonitorHostUrl, true);
+
+            JobManager.Initialize(ObjectFactory.Container.GetInstance<BackupRegistry>());
+            JobManager.JobException += (info) => _logService.WriteLog("An error just happened with a scheduled job: " + info.Exception, true);
+        }
+
+        public static void ApplicationEnd()
+        {
+            _logService.WriteLog("Application Stopped!", true);
+            var client = new WebClient();
+            client.DownloadString(MonitorHostUrl);
+            _logService.WriteLog("Application Shut Down Ping: " + MonitorHostUrl, true);
+        }
+
+        public void Init(HttpApplication application)
+        {
+            application.Error += ApplicationOnError;
+        }
+
+        private void ApplicationOnError(object sender, EventArgs e)
+        {
+            var httpException = ((sender as HttpApplication)?.Server.GetLastError());
+            var exception = httpException?.InnerException;
+            _logService.WriteLog("Unhandled Exception: " + (exception ?? httpException));
+        }
+
+        private static IEnumerable<string> GetCurrentSiteHostAddresses()
+        {
+            string siteName = System.Web.Hosting.HostingEnvironment.SiteName;
+            using (ServerManager serverManager = new ServerManager())
+            {
+                var site = serverManager.Sites.FirstOrDefault(s => s.Name == siteName);
+                return site?.Bindings.Select(b => b.Protocol + "://" + b.Host) ?? Enumerable.Empty<string>();
+            }
+        }
+
+        public void Dispose() { }
+    }
+}
--- WebSite/Global.asax.cs
+++ WebSite/Global.asax.cs
@@ -1,31 +1,23 @@
 ﻿using System;
 using AzureBackupManager.Common;
-using AzureBackupManager.Common.IoC;
-using AzureBackupManager.Scheduling;
-using FluentScheduler;
 
 namespace AzureBackupManager
 {
     public class Global : System.Web.HttpApplication
     {
-        private LogService _logService;
-
         protected void Application_Start(object sender, EventArgs e)
         {
-            ObjectFactory.InitIoC();
+            InitializationModule.ApplicationStart();
+        }
 
-            _logService = ObjectFactory.Container.GetInstance<LogService>();
-            JobManager.Initialize(ObjectFactory.Container.GetInstance<BackupRegistry>());
-            JobManager.JobException += (info) => _logService.WriteLog("An error just happened with a scheduled job: " +  info.Exception);
-
-            _logService.WriteLog("Site started!");
+        protected new void BeginRequest(object sender, EventArgs e)
+        {
         }
 
         protected void Application_End(object sender, EventArgs e)
         {
-            _logService.WriteLog("Site Stopped!");
+            InitializationModule.ApplicationEnd();
         }
     }
-
 
 }
