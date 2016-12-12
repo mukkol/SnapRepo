284efa2267014718cf4a3057561803aa481ab789
SOLITA\mikkohu <mikko.huilaja@solita.fi>
2016-12-12 22:12:20 +0200

    GetUserHostIpAddress refactoring and securing

--- WebSite/Common/AllowedIpAddressValidator.cs
+++ WebSite/Common/AllowedIpAddressValidator.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Configuration;
 using System.Linq;
+using System.Security;
 using System.Security.Principal;
 using System.Web;
 
@@ -28,12 +30,26 @@ namespace SnapRepo.Common
 
         public static string GetUserHostIpAddress(HttpRequestBase request)
         {
-            // Look for a proxy address first
-            var ip = request.ServerVariables["HTTP_X_FORWARDED_FOR"];
-            // If there is no proxy, get the standard remote address
-            if (ip == null || ip.ToLower() == "unknown")
-                ip = request.ServerVariables["REMOTE_ADDR"];
-            return ip;
+            //It's possible to configure the use of HTTP_X_FORWARDED_FOR header in case of proxy server
+            return GetUserHostIpAddress(request,
+                useXForwardedForHeader: bool.Parse(ConfigurationManager.AppSettings["SnapRepo.UseXForwardedForHeader"] ?? "False"),
+                xForwardedProxyIp: ConfigurationManager.AppSettings["SnapRepo.XForwardedProxyIp"]);
+        }
+
+        public static string GetUserHostIpAddress(HttpRequestBase request, bool useXForwardedForHeader, string xForwardedProxyIp)
+        {
+            if (!useXForwardedForHeader)
+                return request.ServerVariables["REMOTE_ADDR"];
+            if(useXForwardedForHeader && string.IsNullOrEmpty(xForwardedProxyIp))
+                throw new ArgumentException("Using HTTP_X_FORWARDED_FOR header without static proxy IP is not safe.", nameof(useXForwardedForHeader));
+            //Make sure that proxy server will force HTTP_X_FORWARDED_FOR header!
+            var ip = request.ServerVariables["REMOTE_ADDR"];
+            var forwardedIp = request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+            if (forwardedIp == null || forwardedIp.ToLower() == "unknown")
+                return ip;
+            if(ip != xForwardedProxyIp)
+                throw new SecurityException($"Can't use HTTP_X_FORWARDED_FOR header IP with request proxy ({xForwardedProxyIp}).");
+            return forwardedIp;
         }
     }
 }
